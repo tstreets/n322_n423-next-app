af12af797d9c4ddced9a7b3ed7faedfccc86d9ce
Ty Streets <=>
2023-03-31 23:13:53 -0400

    Added Basic Routes

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,24 @@
-import '@/styles/globals.css'
+import '@/styles/globals.css';
+import Link from 'next/link';
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+	// console.log('Generating Nav Bar');
+	return (
+		<>
+			<nav>
+				<ul>
+					<li>
+						<Link href='/'>Home</Link>
+					</li>
+					<li>
+						<Link href='/about'>About</Link>
+					</li>
+					<li>
+						<Link href='/products'>Products</Link>
+					</li>
+				</ul>
+			</nav>
+			<Component {...pageProps} />
+		</>
+	);
 }
