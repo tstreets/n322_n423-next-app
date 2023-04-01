3334520768f95f8d6b9d2abf5ee96de8719934aa
Ty Streets <=>
2023-04-01 10:39:59 -0400

    Added remaining code.

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,23 +1,48 @@
-// import '@/styles/globals.css'
+import '../styles/globals.css';
 import Link from 'next/link';
+import NavbarStyles from '../styles/Navbar.module.css';
+import { useRouter } from 'next/router';
+import HeroStyles from '../styles/Hero.module.css';
 
 export default function App({ Component, pageProps }) {
-	// console.log('Generating Nav Bar');
+	const router = useRouter();
+	console.log(router.pathname);
+
+	function getLinkClassName(path) {
+		if (router.pathname === path) {
+			return NavbarStyles.linkActive;
+		} else {
+			return NavbarStyles.linkInactive;
+		}
+	}
+
+	const homeClassName = getLinkClassName('/');
+	const aboutClassName = getLinkClassName('/about');
+	const productsClassName = getLinkClassName('/products');
+
 	return (
 		<>
-			<nav>
+			<nav className={NavbarStyles.navbar}>
 				<ul>
 					<li>
-						<Link href='/'>Home</Link>
+						<Link href='/' className={homeClassName}>
+							Home
+						</Link>
 					</li>
 					<li>
-						<Link href='/about'>About</Link>
+						<Link href='/about' className={aboutClassName}>
+							About
+						</Link>
 					</li>
 					<li>
-						<Link href='/products'>Products</Link>
+						<Link href='/products' className={productsClassName}>
+							Products
+						</Link>
 					</li>
 				</ul>
+				<span className={NavbarStyles.logo}>The Soap Factory</span>
 			</nav>
+			<header className={HeroStyles.hero}></header>
 			<Component {...pageProps} />
 		</>
 	);
